21c6fb0a06d9843818f5ddaa2ad2ab22e33dd2c4
Thomas Rife <112517908+thomas-rife@users.noreply.github.com>
2026-01-27 16:27:02 -0800

    updating roster settings and classname parameters

--- app/classes/[className]/roster/actions.ts
+++ app/classes/[className]/roster/actions.ts
@@ -1,47 +1,56 @@
-'use server'
+"use server";
 
-import { createClient } from '@/utils/supabase/server'
+import { createClient } from "@/utils/supabase/server";
 
 export const enrolledStudents = async (className: string) => {
-  const supabase = createClient()
+  const supabase = createClient();
 
   const {
     data: { user },
-  } = await supabase.auth.getUser()
+  } = await supabase.auth.getUser();
 
   if (!user?.id) {
-    console.error('No user found')
-    return { success: false, error: 'No user found' }
+    console.error("No user found");
+    return { success: false, error: "No user found" };
   }
 
   // clean class name
-  const cleanedClassName = className.replace(/%20/g, ' ')
-  console.log('Cleaned class name: ', cleanedClassName)
+  const cleanedClassName = className.replace(/%20/g, " ");
+  console.log("Cleaned class name: ", cleanedClassName);
 
   const { data: classID, error: classError } = await supabase
-    .from('classes')
-    .select('class_id')
-    .eq('name', cleanedClassName)
-    .single()
+    .from("classes")
+    .select("class_id")
+    .eq("name", cleanedClassName)
+    .single();
 
   if (classError) {
-    console.error('Error fetching class ID: ', classError)
-    return { success: false, error: classError }
+    console.error("Error fetching class ID: ", classError);
+    return { success: false, error: classError };
   }
 
   const { data, error } = await supabase
-    .from('enrollments')
-    .select('student_id')
-    .eq('class_id', classID.class_id)
+    .from("enrollments")
+    .select(
+      `
+    student_id,
+    students (
+      display_name
+    )
+  `,
+    )
+    .eq("class_id", classID.class_id);
 
   if (error) {
-    console.error('Error fetching enrolled students: ', error)
-    return { success: false, error }
+    console.error("Error fetching enrolled students: ", error);
+    return { success: false, error };
   }
 
-  // TODO: this is just temporary data to test the UI
   return {
     success: true,
-    students: data.map(student => ({ ...student, student_name: 'Test Student' })),
-  }
-}
+    students: data.map((student) => ({
+      student_id: student.student_id,
+      display_name: student.students[0].display_name,
+    })),
+  };
+};
